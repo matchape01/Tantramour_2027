68c5222b6c313aac37e0f65651542597c0c71d87
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-09-23 10:25:46 +0200

    [Tantramour 2026] Mise à jour Ateliers.js

--- Ateliers.js
+++ Ateliers.js
@@ -4,7 +4,7 @@
  */
 var ATELIERS = [
   {id:"A_1337",nomFr:"Artistique 1",nomEn:"",animateurs:["R_LINDA"],type:"ATELIER ARTISTIQUE",dureeH:2,dureeMin:0,descFr:"Description en Francais",descEn:"English description",statut:"APPROVED",createdAt:"2026-09-22T08:08:26.268Z",updatedAt:"2026-09-23T08:21:39.511Z"},
-  {id:"A_5826",nomFr:"TEST atelier 1",nomEn:"",animateurs:["R_AUDREY"],type:"ATELIERS TANTRA",dureeH:2,dureeMin:0,descFr:"",descEn:"",statut:"REJECTED",createdAt:"2026-09-22T08:38:13.509Z",updatedAt:"2026-09-22T12:48:34.041Z"},
+  {id:"A_5826",nomFr:"TEST atelier 1",nomEn:"",animateurs:["R_MATTHIEU"],type:"ATELIERS TANTRA",dureeH:2,dureeMin:0,descFr:"",descEn:"",statut:"REJECTED",createdAt:"2026-09-22T08:38:13.509Z",updatedAt:"2026-09-23T08:25:45.948Z"},
   {id:"A_3974",nomFr:"TANTRA 1",nomEn:"",animateurs:["R_AUDREY"],type:"ATELIERS TANTRA",dureeH:1,dureeMin:0,descFr:"",descEn:"",statut:"APPROVED",createdAt:"2026-09-22T09:18:43.486Z",updatedAt:"2026-09-23T08:21:56.407Z"},
   {id:"A_5692",nomFr:"Tantra Café",nomEn:"",animateurs:["R_SANDRINE"],type:"TANTRA CAFE",dureeH:1,dureeMin:0,descFr:"",descEn:"",statut:"APPROVED",createdAt:"2026-09-22T10:01:27.960Z",updatedAt:"2026-09-23T08:22:31.250Z"},
   {id:"A_4797",nomFr:"Love Temple 1",nomEn:"dgsdg",animateurs:["R_CLEMENT"],type:"LOVE TEMPLE",dureeH:1,dureeMin:0,descFr:"dsgsdg",descEn:"sgdsgds",statut:"APPROVED",createdAt:"2026-09-22T10:12:35.270Z",updatedAt:"2026-09-23T08:25:36.525Z"},
